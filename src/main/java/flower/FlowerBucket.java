897c4298004a6519bb56f915b2ccb6cb933f7513
PavloKukurik <116553132+PavloKukurik@users.noreply.github.com>
2023-10-12 19:00:35 +0300

    Initial commit

---
+++ src/main/java/flower/FlowerBucket.java
@@ -0,0 +1,28 @@
+package flower;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowerBucket {
+    private final List<FlowerPack> flowerPacks;
+
+    public FlowerBucket() {
+        this.flowerPacks = new ArrayList<>();
+    }
+
+    public void add(FlowerPack flowerPack) {
+        flowerPacks.add(flowerPack);
+    }
+
+    public int getPrice() {
+        int total = 0;
+        for (FlowerPack pack : flowerPacks) {
+            total += pack.flower().getPrice() * pack.quantity();
+        }
+        return total;
+    }
+}
+
+record FlowerPack(Flower flower, int quantity) {}
+
+class Rose extends Flower {}
